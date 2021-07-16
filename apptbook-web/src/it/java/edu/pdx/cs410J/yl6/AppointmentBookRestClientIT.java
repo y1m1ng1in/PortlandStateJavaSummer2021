1079285cc42b0820a935a43346f48b278eefc5fb
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2021-07-16 13:54:29 -0700

    init project4

---
+++ apptbook-web/src/it/java/edu/pdx/cs410J/yl6/AppointmentBookRestClientIT.java
@@ -0,0 +1,61 @@
+package edu.pdx.cs410J.yl6;
+
+import edu.pdx.cs410J.web.HttpRequestHelper;
+import org.junit.jupiter.api.MethodOrderer.MethodName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Integration test that tests the REST calls made by {@link AppointmentBookRestClient}
+ */
+@TestMethodOrder(MethodName.class)
+class AppointmentBookRestClientIT {
+  private static final String HOSTNAME = "localhost";
+  private static final String PORT = System.getProperty("http.port", "8080");
+
+  private AppointmentBookRestClient newAppointmentBookRestClient() {
+    int port = Integer.parseInt(PORT);
+    return new AppointmentBookRestClient(HOSTNAME, port);
+  }
+
+  @Test
+  void test0RemoveAllDictionaryEntries() throws IOException {
+    AppointmentBookRestClient client = newAppointmentBookRestClient();
+    client.removeAllDictionaryEntries();
+  }
+
+  @Test
+  void test1EmptyServerContainsNoDictionaryEntries() throws IOException {
+    AppointmentBookRestClient client = newAppointmentBookRestClient();
+    Map<String, String> dictionary = client.getAllDictionaryEntries();
+    assertThat(dictionary.size(), equalTo(0));
+  }
+
+  @Test
+  void test2DefineOneWord() throws IOException {
+    AppointmentBookRestClient client = newAppointmentBookRestClient();
+    String testWord = "TEST WORD";
+    String testDefinition = "TEST DEFINITION";
+    client.addDictionaryEntry(testWord, testDefinition);
+
+    String definition = client.getDefinition(testWord);
+    assertThat(definition, equalTo(testDefinition));
+  }
+
+  @Test
+  void test4MissingRequiredParameterReturnsPreconditionFailed() throws IOException {
+    AppointmentBookRestClient client = newAppointmentBookRestClient();
+    HttpRequestHelper.Response response = client.postToMyURL(Map.of());
+    assertThat(response.getContent(), containsString(Messages.missingRequiredParameter("word")));
+    assertThat(response.getCode(), equalTo(HttpURLConnection.HTTP_PRECON_FAILED));
+  }
+
+}
---
+++ apptbook-web/src/main/java/edu/pdx/cs410J/yl6/AppointmentBookRestClient.java
@@ -0,0 +1,72 @@
+package edu.pdx.cs410J.yl6;
+
+import com.google.common.annotations.VisibleForTesting;
+import edu.pdx.cs410J.web.HttpRequestHelper;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+
+/**
+ * A helper class for accessing the rest client.
+ */
+public class AppointmentBookRestClient extends HttpRequestHelper {
+  private static final String WEB_APP = "apptbook";
+  private static final String SERVLET = "appointments";
+
+  private final String url;
+
+  /**
+   * Creates a client to the appointment book REST service running on the given host and port
+   *
+   * @param hostName The name of the host
+   * @param port     The port
+   */
+  public AppointmentBookRestClient(String hostName, int port) {
+    this.url = String.format("http://%s:%d/%s/%s", hostName, port, WEB_APP, SERVLET);
+  }
+
+  /**
+   * Returns all dictionary entries from the server
+   */
+  public Map<String, String> getAllDictionaryEntries() throws IOException {
+    Response response = get(this.url, Map.of());
+    return Messages.parseDictionary(response.getContent());
+  }
+
+  /**
+   * Returns the definition for the given word
+   */
+  public String getDefinition(String word) throws IOException {
+    Response response = get(this.url, Map.of("word", word));
+    throwExceptionIfNotOkayHttpStatus(response);
+    String content = response.getContent();
+    return Messages.parseDictionaryEntry(content).getValue();
+  }
+
+  public void addDictionaryEntry(String word, String definition) throws IOException {
+    Response response = postToMyURL(Map.of("word", word, "definition", definition));
+    throwExceptionIfNotOkayHttpStatus(response);
+  }
+
+  @VisibleForTesting
+  Response postToMyURL(Map<String, String> dictionaryEntries) throws IOException {
+    return post(this.url, dictionaryEntries);
+  }
+
+  public void removeAllDictionaryEntries() throws IOException {
+    Response response = delete(this.url, Map.of());
+    throwExceptionIfNotOkayHttpStatus(response);
+  }
+
+  private Response throwExceptionIfNotOkayHttpStatus(Response response) {
+    int code = response.getCode();
+    if (code != HTTP_OK) {
+      String message = response.getContent();
+      throw new RestException(code, message);
+    }
+    return response;
+  }
+
+}
